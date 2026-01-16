7ae4a2186084f9322a8c0553fa5fed8fbfb60a4e
CapCode111 <aryansharma20035@gmail.com>
2026-01-16 11:57:44 +0530

    Initial Commit

---
+++ Array/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public static void main(String[]args){
+
+    int [] arr = new int[10];
+    arr[3] = 32;
+    arr[2] = 4;
+    for(int i =0; i < arr.length; i++){
+        System.out.println(arr[i]);
+    }
+}
